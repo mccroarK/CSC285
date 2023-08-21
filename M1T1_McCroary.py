392546a2d0434677e2d554f1eb02f7d491e4f258
mccroarK <55157272+mccroarK@users.noreply.github.com>
2023-08-21 19:40:06 -0400

    Add files via upload

---
+++ M1T1_McCroary.py
@@ -0,0 +1,15 @@
+# CTS 285
+# M1T1
+# Kevin McCroary
+# 8/21/2023
+
+def main():
+    print("My name is Kevin McCroary.")
+    print("I am still learning C#. I have learned Python, Java, and a miniscule amount of HTML. Most of these languages I have since forgotten.")
+    print("There are rare chances where I dabble with art programs.")
+    print("My partner for the first homework assignment is Gina Jordan.")
+
+    # Input to not close command prompt
+    input()
+
+main()
